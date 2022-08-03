6b6e6058a556428ced52b66ab13c98238404852a
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-03 18:27:15 +0530

    J8_03_Comparator_Compare +

---
+++ Java_8_Features/src/info/ashutosh/J8_03_Comparator_Compare.java
@@ -0,0 +1,41 @@
+package info.ashutosh;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class J8_03_Comparator_Compare implements Comparator<Integer> {
+
+	public static void main(String[] args) {
+
+		// populating Array List
+		ArrayList<Integer> arrayList = new ArrayList<Integer>();
+		arrayList.add(5);
+		arrayList.add(1);
+		arrayList.add(2);
+		arrayList.add(3);
+		arrayList.add(6);
+		arrayList.add(4);
+
+		// Print Before Sorting
+		System.out.println(arrayList);
+
+		// sort method will take list and Comparator. Comparator for customized Sorting.
+		Collections.sort(arrayList, new J8_03_Comparator_Compare());
+
+		// Print After Sorting
+		System.out.println(arrayList);
+	}
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		if (o1 > o2) {
+			return -1;
+		} else if (o1 < 02) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+}
