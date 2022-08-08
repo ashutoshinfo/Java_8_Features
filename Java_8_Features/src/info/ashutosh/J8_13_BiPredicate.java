f5b7e95c874a15b7fc3a1a17cb82ceae0f9bf13e
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 13:27:52 +0530

    /* take two arguments <Arguments,Return Type> */

--- Java_8_Features/src/info/ashutosh/J8_13_BiPredicate.java
+++ Java_8_Features/src/info/ashutosh/J8_13_BiPredicate.java
@@ -4,7 +4,7 @@ import java.util.function.BiPredicate;
 
 public class J8_13_BiPredicate {
 	public static void main(String[] args) {
-		/* take two arguments */
+		/* take two arguments <Arguments,Return Type> */
 		BiPredicate<Integer, Integer> biPredicate = (i, j) -> (i + j) % 2 == 0;
 
 		System.out.println(biPredicate.test(1, 2));
