a769296ce479be1e235ce46b15e0ee080d6c2299
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 13:27:32 +0530

    /* take two arguments <Arguments,Arguments,Return Type> */

--- Java_8_Features/src/info/ashutosh/J8_14_BiFuction.java
+++ Java_8_Features/src/info/ashutosh/J8_14_BiFuction.java
@@ -4,7 +4,7 @@ import java.util.function.BiFunction;
 
 public class J8_14_BiFuction {
 	public static void main(String[] args) {
-		/* take two arguments */
+		/* take two arguments <Arguments,Arguments,Return Type> */
 		BiFunction<Integer, Integer, Integer> biPredicate_sum = (i, j) -> (i + j);
 
 		System.out.println(biPredicate_sum.apply(1, 2));
