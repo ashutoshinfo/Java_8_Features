8f9e8a40c24eaadb8cbd98c548220cbb2db94bb0
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 12:36:49 +0530

    J8_12_Supplier_6_OTP ++

---
+++ Java_8_Features/src/info/ashutosh/J8_12_Supplier_6_OTP.java
@@ -0,0 +1,15 @@
+package info.ashutosh;
+
+import java.util.function.Supplier;
+
+public class J8_11_Supplier_6_OTP {
+	public static void main(String[] args) {
+		Supplier<String> supplier = () -> {
+			String[] names = { "Ashutosh", "Kruti", "Jayesh", "Kokila" };
+			int random = (int) (Math.random() * 4);
+			return names[random];
+		};
+		System.out.println(supplier.get());
+	}
+
+}
