b23d61e65bfe4f6803e7a1e40ff07c2e60b042ff
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 12:36:01 +0530

    J8_11_Supplier +

--- Java_8_Features/src/info/ashutosh/J8_11_Supplier.java
+++ Java_8_Features/src/info/ashutosh/J8_11_Supplier.java
@@ -5,11 +5,19 @@ import java.util.function.Supplier;
 public class J8_11_Supplier {
 	public static void main(String[] args) {
 		Supplier<String> supplier = () -> {
-			String[] names = { "Ashutosh", "Kruti", "Jayesh", "Kokila" };
-			int random = (int) (Math.random() * 4);
-			return names[random];
+			String aToZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&";
+			Supplier<Integer> randomInt = () -> (int) (Math.random() * aToZ.length());
+			String finalOtpString = "";
+			for (int i = 0; i < 6; i++) {
+				finalOtpString = finalOtpString + aToZ.charAt(randomInt.get());
+
+			}
+
+			return finalOtpString;
 		};
+
 		System.out.println(supplier.get());
+
 	}
 
 }
