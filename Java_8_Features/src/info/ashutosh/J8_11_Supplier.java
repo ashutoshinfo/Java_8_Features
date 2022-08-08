a415c1e75f19940afb4149c9aad6f68bb36d793e
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 12:37:59 +0530

    Update

--- Java_8_Features/src/info/ashutosh/J8_11_Supplier.java
+++ Java_8_Features/src/info/ashutosh/J8_11_Supplier.java
@@ -5,19 +5,11 @@ import java.util.function.Supplier;
 public class J8_11_Supplier {
 	public static void main(String[] args) {
 		Supplier<String> supplier = () -> {
-			String aToZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&";
-			Supplier<Integer> randomInt = () -> (int) (Math.random() * aToZ.length());
-			String finalOtpString = "";
-			for (int i = 0; i < 6; i++) {
-				finalOtpString = finalOtpString + aToZ.charAt(randomInt.get());
-
-			}
-
-			return finalOtpString;
+			String[] names = { "Ashutosh", "Kruti", "Jayesh", "Kokila" };
+			int random = (int) (Math.random() * 4);
+			return names[random];
 		};
-
 		System.out.println(supplier.get());
-
 	}
 
 }
--- Java_8_Features/src/info/ashutosh/J8_12_Supplier_6_OTP.java
+++ Java_8_Features/src/info/ashutosh/J8_12_Supplier_6_OTP.java
@@ -2,14 +2,22 @@ package info.ashutosh;
 
 import java.util.function.Supplier;
 
-public class J8_11_Supplier_6_OTP {
+public class J8_12_Supplier_6_OTP {
 	public static void main(String[] args) {
 		Supplier<String> supplier = () -> {
-			String[] names = { "Ashutosh", "Kruti", "Jayesh", "Kokila" };
-			int random = (int) (Math.random() * 4);
-			return names[random];
+			String aToZ = "ABCDEFGHIJKLMNOPQRSTUVWXYZ@#$&";
+			Supplier<Integer> randomInt = () -> (int) (Math.random() * aToZ.length());
+			String finalOtpString = "";
+			for (int i = 0; i < 6; i++) {
+				finalOtpString = finalOtpString + aToZ.charAt(randomInt.get());
+
+			}
+
+			return finalOtpString;
 		};
+
 		System.out.println(supplier.get());
+
 	}
 
 }
