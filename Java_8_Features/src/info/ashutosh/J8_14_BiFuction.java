3bc3fea44cf06b0df0b7dc113e8d6cf5e06e2103
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 13:26:26 +0530

    J8_14_BiFuction +

---
+++ Java_8_Features/src/info/ashutosh/J8_14_BiFuction.java
@@ -0,0 +1,14 @@
+package info.ashutosh;
+
+import java.util.function.BiFunction;
+
+public class J8_14_BiFuction {
+	public static void main(String[] args) {
+		/* take two arguments */
+		BiFunction<Integer, Integer, Integer> biPredicate_sum = (i, j) -> (i + j);
+
+		System.out.println(biPredicate_sum.apply(1, 2));
+
+	}
+
+}
