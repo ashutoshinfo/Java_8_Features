8f79257f867c3cbf58cc98aa5730d140ffab93de
ashutoshjp <ashutoshpatel5@yahoo.in>
2022-08-08 18:05:32 +0530

    J8_19_ToIntFunction +

---
+++ Java_8_Features/src/info/ashutosh/J8_19_ToIntFunction.java
@@ -0,0 +1,12 @@
+package info.ashutosh;
+
+import java.util.function.ToIntFunction;
+
+public class J8_19_ToIntFunction {
+	public static void main(String[] args) {
+		/* take 'Integer' as a argument and return 'int' */
+		ToIntFunction<Integer> toIntFunction = (i) -> i * i;
+		System.out.println(toIntFunction.applyAsInt(10));
+
+	}
+}
